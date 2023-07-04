7061485228a502d5f8895edb537de258fcaac5a2
wujuno <huj88854@gmail.com>
2023-07-04 16:09:35 +0900

    Create InitializeRedis()

---
+++ pkg/redisrepo/client.go
@@ -0,0 +1,30 @@
+package redisrepo
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/go-redis/redis/v8"
+)
+
+var redisClient *redis.Client
+
+func InitializeRedis() *redis.Client {
+	conn := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("REDIS_CONNECTION_STRING"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB: 0,
+	})
+
+	pong, err := conn.Ping(context.Background()).Result()
+	if err != nil {
+		log.Fatal("Redis Connection Failed 🤢", err)
+	}
+
+	log.Println("Redis Successfully Connected. 😉", "Ping", pong)
+
+	redisClient = conn
+
+	return redisClient
+}
